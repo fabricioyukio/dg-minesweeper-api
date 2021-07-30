ed89849b336458f898dfd46b3581306cd255de4d
Fabricio Yukio <fabricioyukio@gmail.com>
2021-07-30 09:56:47 -0300

    added game events registration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_29_131243) do
+ActiveRecord::Schema.define(version: 2021_07_30_120351) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,14 @@ ActiveRecord::Schema.define(version: 2021_07_29_131243) do
     t.index ["name"], name: "index_games_on_name", unique: true
   end
 
+  create_table "logs", force: :cascade do |t|
+    t.string "event"
+    t.string "description"
+    t.bigint "game_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["game_id"], name: "index_logs_on_game_id"
+  end
+
+  add_foreign_key "logs", "games"
 end
